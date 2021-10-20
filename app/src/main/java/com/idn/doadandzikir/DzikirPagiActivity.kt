01524f124f6b3261177290262f6ffef9d0d60caa
Aqila Ramdhn <aqilarmdhn23@Aqilas-MacBook-Pro.local>
2021-10-20 10:48:22 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/doadandzikir/DzikirPagiActivity.kt
@@ -0,0 +1,11 @@
+package com.idn.doadandzikir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DzikirPagiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dzikir_pagi)
+    }
+}
